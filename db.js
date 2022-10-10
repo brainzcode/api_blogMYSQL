719f97cb439c3fb65bc0531b7f556a483d3e8776
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-10-10 03:15:20 +0100

    first commit

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ import mysql from 'mysql';
 export const db = mysql.createConnection({
   host: 'localhost',
 	user: 'root',
-	password: process.env.REACT_APP_PASSWORD,
-	database: process.env.REACT_APP_DATABASE,
+	password: process.env.APP_PASSWORD,
+	database: process.env.APP_DATABASE,
 })
